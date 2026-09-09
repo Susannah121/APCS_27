747d8e23c858624dea2fe591946864344736e072
Susannah121 <susannahlebsock@gmail.com>
2026-09-09 21:18:10 +0000

    update

--- 000_Labs/006_doubles/base_code/starter.java
+++ 000_Labs/006_doubles/base_code/starter.java
@@ -1,6 +1,6 @@
 /*
- *	Author:  
- *  Date: 
+ *	Author:  Susannah Lebsock
+ *  Date: 9/8/26
 */
 
 import java.util.Scanner;
@@ -9,6 +9,9 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		double fahrenheit = 32.0;
+		double celsius = (fahrenheit - 32)*5/9 ;
+
+		System.out.print("The Celsius value for " + fahrenheit + " degrees Fahrenheit is " + celsius + " degrees"); 
 	}
 }
